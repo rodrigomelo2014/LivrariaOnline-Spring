dbef11059467a577876422cb4c70762cc44e8b91
spring6924 <spring6924@caelum114-07>
2017-07-07 21:51:54 -0300

    Respondendo Requisição com Json

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -1,8 +1,12 @@
 package br.com.casadocodigo.loja.conf;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,17 +15,21 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
+import com.google.common.cache.CacheBuilder;
 import br.com.casadocodigo.loja.controllers.HomeController;
 import br.com.casadocodigo.loja.daos.InterfaceDAO;
 import br.com.casadocodigo.loja.daos.ProductDAO;
 import br.com.casadocodigo.loja.infra.FileSaver;
 import br.com.casadocodigo.loja.models.ShoppingCart;
+import br.com.casadocodigo.loja.viewresolver.JsonViewResolver;
 
 @EnableCaching
 @EnableWebMvc
@@ -68,6 +76,25 @@ public class AppWebConfiguration {
 	
 	@Bean
 	public CacheManager cacheManager(){
-		return new ConcurrentMapCacheManager();
+		CacheBuilder<Object, Object> builder = CacheBuilder.newBuilder()
+														   .maximumSize(100)
+														   .expireAfterAccess(5, TimeUnit.MINUTES);
+		GuavaCacheManager cacheManager = new GuavaCacheManager();
+		cacheManager.setCacheBuilder(builder);
+		
+		return cacheManager;
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager){
+		List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+		
+		resolvers.add(internalResourceViewResolver());
+		resolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+		return resolver;
 	}
 }
--- casadocodigo/src/main/webapp/WEB-INF/views/products/show.jsp
+++ casadocodigo/src/main/webapp/WEB-INF/views/products/show.jsp
@@ -16,17 +16,17 @@
 	
 	<link rel="icon" href="//cdn.shopify.com/s/files/1/0155/7645/t/177/assets/favicon.ico?11981592617154272979" type="image/ico" />
 	<link href="https://plus.googlecom/108540024862647200608" rel="publisher"/>
-	<link href="../css/cssbase-min.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/cssbase-min.css" rel="stylesheet" type="text/css" media="all" />
 	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet'/>
-	<link href="../css/fonts.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../css/fontello-ie7.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../css/fontello-embedded.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../css/fontello.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../css/style.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../css/layout-colors.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../css/responsive-style.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../css/guia-do-programador-style.css" rel="stylesheet" type="text/css"  media="all"  />
-    <link href="../css/produtos.css" rel="stylesheet" type="text/css"  media="all"  />
+	<link href="casadocodigo/resources/css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/fontello-ie7.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/fontello-embedded.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/fontello.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/layout-colors.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/responsive-style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="casadocodigo/resources/css/guia-do-programador-style.css" rel="stylesheet" type="text/css"  media="all"  />
+    <link href="casadocodigo/resources/css/produtos.css" rel="stylesheet" type="text/css"  media="all"  />
 	<link rel="canonical" href="http://www.casadocodigo.com.br/" />
 
 </head>
